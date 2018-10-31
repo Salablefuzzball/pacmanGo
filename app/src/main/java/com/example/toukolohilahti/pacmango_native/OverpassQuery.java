4f34520c8038d7765af025bc82bb44ba75c196d7
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-10-31 20:13:37 +0200

    Added asyncTask to handle queries

--- app/src/main/java/com/example/toukolohilahti/pacmango_native/OverpassQuery.java
+++ app/src/main/java/com/example/toukolohilahti/pacmango_native/OverpassQuery.java
@@ -1,4 +1,51 @@
 package com.example.toukolohilahti.pacmango_native;
 
-public class OverpassQuery {
+import android.os.AsyncTask;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class OverpassQuery extends AsyncTask<String, String, String> {
+
+    public OverpassQuery(){
+        //set context variables if required
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String urlString = params[0]; // URL to call
+        String data = params[1]; //data to post
+        OutputStream out = null;
+
+        try {
+            URL url = new URL(urlString);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            out = new BufferedOutputStream(urlConnection.getOutputStream());
+
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(out, "UTF-8"));
+            writer.write(data);
+            writer.flush();
+            writer.close();
+            out.close();
+
+            urlConnection.connect();
+            if(urlConnection.getResponseCode() == 200) {
+                return urlConnection.getResponseMessage();
+            } else {
+                return "";
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return "";
+        }
+    }
 }
