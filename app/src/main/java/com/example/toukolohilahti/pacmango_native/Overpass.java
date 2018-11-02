a84839ea2105396d520f6529229ccfc98019f41f
Aaro Alhainen <aaro.alhainen@gmail.com>
2018-11-02 18:19:26 +0200

    Added calculations for distace and middle point

--- app/src/main/java/com/example/toukolohilahti/pacmango_native/Overpass.java
+++ app/src/main/java/com/example/toukolohilahti/pacmango_native/Overpass.java
@@ -36,6 +36,16 @@ public class Overpass {
             this.type = type;
             this.id = id;
 
+
+            this.geometry = this.convertToPositionArray(geometry);
+        }
+
+        /**
+         * Convert given JSONArray to position arraylist.
+         * @param geometry JSONArray containing JSONObjects of gps locations.
+         * @return Position arrayList.
+         */
+        private ArrayList<Position> convertToPositionArray(JSONArray geometry) {
             ArrayList<Position> posArray = new ArrayList<>();
             for (int index = 0; index < geometry.length(); index++) {
                 JSONObject geometryObj = null;
@@ -47,8 +57,51 @@ public class Overpass {
                     e.printStackTrace();
                 }
             }
-            this.geometry = posArray;
+            return posArray;
         }
+
+        private Position calculateMidPoint(Position pos1, Position pos2){
+
+            double dLon = Math.toRadians(pos2.lon - pos1.lon);
+
+            //convert to radians
+            double lat1 = Math.toRadians(pos1.lat);
+            double lat2 = Math.toRadians(pos2.lat);
+            double lon1 = Math.toRadians(pos1.lon);
+
+            double Bx = Math.cos(lat2) * Math.cos(dLon);
+            double By = Math.cos(lat2) * Math.sin(dLon);
+            double lat3 = Math.atan2(Math.sin(lat1) + Math.sin(lat2), Math.sqrt((Math.cos(lat1) + Bx) * (Math.cos(lat1) + Bx) + By * By));
+            double lon3 = lon1 + Math.atan2(By, Math.cos(lat1) + Bx);
+
+            return new Position(lat3, lon3);
+        }
+
+        /**
+         * Calculate distance between two locations.
+         * @param pos1 Position of the first place.
+         * @param pos2 Position of the second place.
+         * @return Returns distance in meters.
+         */
+        private double distance(Position pos1, Position pos2) {
+
+            final int R = 6371; // Radius of the earth
+
+            double latDistance = Math.toRadians(pos2.lat - pos1.lat);
+            double lonDistance = Math.toRadians(pos2.lon - pos1.lon);
+            double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                    + Math.cos(Math.toRadians(pos1.lat)) * Math.cos(Math.toRadians(pos2.lat))
+                    * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+            double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+            double distance = R * c * 1000; // convert to meters
+
+            double height = 0;
+
+            distance = Math.pow(distance, 2) + Math.pow(height, 2);
+
+            return Math.sqrt(distance);
+        }
+
 
         @Override
         public String toString() {
